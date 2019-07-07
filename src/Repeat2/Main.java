4acc5296ec28ecffe9ddd00c32704b09166ace04
Rasskopovaa <raskopovaa@gmail.com>
2019-07-07 19:53:56 +0700

    Задание print3 (2)

---
+++ src/Repeat2/Main.java
@@ -0,0 +1,13 @@
+package Repeat2;
+
+public class Main {
+    public static void print3(String s) {
+        System.out.println(s + " " + s + " " + s);
+    }
+
+    public static void main(String[] args) {
+        print3("window");
+        print3("file");
+    }
+}
+
