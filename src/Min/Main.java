032d696da4e43c905bba56c00c835408697add51
Rasskopovaa <raskopovaa@gmail.com>
2019-07-07 20:46:26 +0700

    Задание на минимум

---
+++ src/Min/Main.java
@@ -0,0 +1,13 @@
+package Min;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++)
+            System.out.println(Math.min(123, 466));
+    }
+
+    public static int min(int a, int b) {
+        return min(a, b);
+    }
+}
+
