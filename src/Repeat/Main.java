1dfe5133ca5051da40b1cbe4994c3029545bbfc3
Rasskopovaa <raskopovaa@gmail.com>
2019-07-07 19:06:31 +0700

    Задание print3

---
+++ src/Repeat/Main.java
@@ -0,0 +1,16 @@
+package Repeat;
+
+public class Main {
+
+    public static void print3(String s) {
+        for (int i = 0; i < 3; i++)
+            System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        print3("I love you!");
+    }
+
+
+}
+
