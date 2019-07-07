b6504412bb54c82cdab69ec37cec42b88aedcc39
Rasskopovaa <raskopovaa@gmail.com>
2019-07-08 00:52:28 +0700

    минимум 3 чисел

--- src/Min/Main.java
+++ src/Min/Main.java
@@ -3,12 +3,12 @@ package Min;
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 3; i++)
-            System.out.println(Math.min(24, 56));
+            System.out.println(Math.min(Math.min(674, 697), 876));
     }
 
-    public static int min(int a, int b) {
-        return min(a, b);
+    public static int min(int a, int b, int c) {
 
+        return Math.min(Math.min(a, b), c);
     }
 }
 
