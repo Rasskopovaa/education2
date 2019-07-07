8c40c5ef6a2c34e034e521078153f10e91280757
Rasskopovaa <raskopovaa@gmail.com>
2019-07-07 21:41:10 +0700

    Задание на минимум (2)

--- src/Min/Main.java
+++ src/Min/Main.java
@@ -2,12 +2,19 @@ package Min;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++)
-            System.out.println(Math.min(123, 466));
+        for (int i = 0; i < 4; i++)
+            System.out.println(min(343, 434, 634));
     }
 
-    public static int min(int a, int b) {
-        return min(a, b);
+    public static int min(int a, int b, int c) {
+        if (a < b && a < c) {
+            return a;
+        } else if (b < a && b < c) {
+            return b;
+        } else {
+            return c;
+        }
+
     }
 }
 
